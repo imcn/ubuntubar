661948b5eb3f9ddce83a60382a7149395ddd635f
yi du <chinacnd@163.com>
2024-01-13 17:08:04 +0800

    首次创建代码文件

---
+++ mymodule.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import os
+# mergefile_csv 合并csv GBK文件
+
+
+def mfg(path):    # 参数为文件路径 路径样式为：E:\\年度医保基础数据\\2023年
+    data = pd.DataFrame()
+    files = os.listdir(path)
+    for file in files:
+        yb_data = pd.read_csv(f'{path}\\{file}', encoding='GBK')
+        data = pd.concat([data,yb_data], axis=0, ignore_index=True)
+    return data
+
+# mergefile_utf 合并csv utf-8文件
+
+
+def mfu(path):    # 参数为文件路径 路径样式为：E:\\年度医保基础数据\\2023年
+    data = pd.DataFrame()
+    files = os.listdir(path)
+    for file in files:
+        yb_data = pd.read_csv(f'{path}\\{file}', encoding='utf-8')
+        data = pd.concat([data,yb_data], axis=0, ignore_index=True)
+    return data
+
+# mergefile_utf 合并excel文件
+
+
+def mf(path):    # 参数为文件路径 路径样式为：E:\\年度医保基础数据\\2023年
+    data = pd.DataFrame()
+    files = os.listdir(path)
+    for file in files:
+        yb_data = pd.read_excel(f'{path}\\{file}')
+        data = pd.concat([data,yb_data], axis=0, ignore_index=True)
+    return data
+
+# 对象测试
+
+class Imcn:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def sum(self):
+        return self.a + self.b + self.c
+    
+    def mb(self):
+        return self.a * self.b * self.c
